d20728762ad3b39b7d6b2a48b87baa9af6b06627
Chanon Roy <chanonroy@gmail.com>
2020-01-18 23:36:49 +0000

    remove console message

--- learnyounode/filtered-ls.js
+++ learnyounode/filtered-ls.js
@@ -24,7 +24,6 @@ const ext = '.' + process.argv[3];
 
 fs.readdir(folder, (err, files) => {
   if (err) throw err;
-  // console.log(files);
   files.forEach(file => {
     if (path.extname(file) === ext) {
       console.log(file);
