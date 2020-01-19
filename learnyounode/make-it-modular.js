792df5f870bcb3b15aeb71146a2a850f78984f4e
Chanon Roy <chanonroy@gmail.com>
2020-01-19 10:44:51 +0000

    feat: make it modular

---
+++ learnyounode/make-it-modular.js
@@ -0,0 +1,10 @@
+var filterFn = require('./mymodule.js')
+var dir = process.argv[2]
+var filterStr = process.argv[3]
+
+filterFn(dir, filterStr, function (err, list) {
+  if (err) throw err;
+  list.forEach(function (file) {
+    console.log(file)
+  })
+})
